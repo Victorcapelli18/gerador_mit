888d88980a71763a7b428fc4ebb541ad8dadff92
Victor <victorcapelli18@gmail.com>
2025-04-20 09:47:46 -0300

    adicionado cnpj_raiz na entidade company

--- src/domain/entities/company_entity.py
+++ src/domain/entities/company_entity.py
@@ -3,6 +3,7 @@ from pydantic import BaseModel, Field
 
 class Company(BaseModel):
     codigo_sap: str
+    cnpj_raiz: str
     cnpj: str = Field(..., min_length=14, max_length=14)
     qualificacao_pj: str
     tributacao_lucro: str
